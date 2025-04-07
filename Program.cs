fe32c9e50417fee3f4e03dcdcbb66c36204c39db
KachleGG <drztesiklobouky822@gmail.com>
2025-04-07 18:41:47 +0200

    1.0.3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
         
         static void Main(string[] args) {
             // Initialize updater and try to update
-            Updater updater = new Updater("KachleGG", "versionControl", "1.0.4", "Updatr");
+            Updater updater = new Updater("KachleGG", "versionControl", "1.0.3", "Updatr");
             updater.Update();
         }
     }
