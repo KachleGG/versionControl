22d5ea21ac5937c6e968c9579f37feeb057a4701
KachleGG <drztesiklobouky822@gmail.com>
2025-04-07 21:20:14 +0200

    1.1.2 - fixed namespace issues

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Updater {
         
         static void Main(string[] args) {
             // Initialize updater and try to update
-            Updater updater = new Updater("KachleGG", "versionControl", "1.1.1", "Updatr");
+            Updater updater = new Updater("KachleGG", "versionControl", "1.1.2", "Updatr");
             updater.Update();
         }
     }
--- Updater.cs
+++ Updater.cs
@@ -8,7 +8,7 @@ using System.IO;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace Updater {
+namespace UpdateMGR {
     class Updater {
         private readonly string _username;
         private readonly string _repository;
